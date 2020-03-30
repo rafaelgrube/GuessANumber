2034c1e90fece1e52229a41ae589baaa6f68be41
Rafael Grube <rafael.grube@solvertecnologia.com.br>
2020-03-30 12:28:23 -0300

    Ajustements for GameOverScreen layout orientation

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -24,70 +24,70 @@ const GameOverScreen = props => {
 
   // View
   return (
-    <ScrollView>
-        <View style={styles.screen}>
-          <TitleText>The Game is Over!</TitleText>
-          <View
-            style={{
-              ...styles.imageContainer,
-              ...{
-                width:
-                  avaiableDeviceWidth > 350
-                    ? avaiableDeviceWidth * 0.3
-                    : avaiableDeviceWidth * 0.7,
-                height:
-                  avaiableDeviceWidth > 350
-                    ? avaiableDeviceWidth * 0.3
-                    : avaiableDeviceWidth * 0.7,
-                borderRadius:
-                  (avaiableDeviceWidth > 350
-                    ? avaiableDeviceWidth * 0.3
-                    : avaiableDeviceWidth * 0.7) / 2,
-                marginBottom: avaiableDeviceHeight / 30
-              }
-            }}
-          >
-            <Image
-              source={require("../assets/success.png")}
-              // source={{ uri: 'https://www.mountain-forecast.com/images/mtn_home.jpg' }}
-              style={styles.image}
-              resizeMode="cover"
-            />
-          </View>
-          <View
-            style={{
-              ...styles.resultContainer,
-              ...{ marginVertical: avaiableDeviceHeight / 60 }
-            }}
-          >
-            <BodyText
-              style={{
-                ...styles.resultText,
-                ...{ fontSize: avaiableDeviceHeight < 400 ? 16 : 20 }
-              }}
-            >
-              Your phone needed{" "}
-              <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds
-              to guess the number{" "}
-              <Text style={styles.highlight}>{props.userNumber}</Text>.
-            </BodyText>
-          </View>
-          <PrimaryButton onPress={props.onRestart}>New Game</PrimaryButton>
+    <ScrollView style={styles.screenContainer}>
+      <View style={styles.screen}>
+        <TitleText>The Game is Over!</TitleText>
+        <View
+          style={{
+            ...styles.imageContainer,
+            ...{
+              width:
+                avaiableDeviceWidth > 350
+                  ? avaiableDeviceWidth * 0.3
+                  : avaiableDeviceWidth * 0.7,
+              height:
+                avaiableDeviceWidth > 350
+                  ? avaiableDeviceWidth * 0.3
+                  : avaiableDeviceWidth * 0.7,
+              borderRadius:
+                (avaiableDeviceWidth > 350
+                  ? avaiableDeviceWidth * 0.3
+                  : avaiableDeviceWidth * 0.7) / 2,
+              marginBottom: avaiableDeviceHeight / 30
+            }
+          }}
+        >
+          <Image
+            source={require("../assets/success.png")}
+            // source={{ uri: 'https://www.mountain-forecast.com/images/mtn_home.jpg' }}
+            style={styles.image}
+            resizeMode="cover"
+          />
         </View>
-      </ScrollView>
+        <View
+          style={{
+            ...styles.resultContainer,
+            ...{ marginVertical: avaiableDeviceHeight / 60 }
+          }}
+        >
+          <BodyText
+            style={{
+              ...styles.resultText,
+              ...{ fontSize: avaiableDeviceHeight < 400 ? 16 : 20 }
+            }}
+          >
+            Your phone needed{" "}
+            <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
+            guess the number{" "}
+            <Text style={styles.highlight}>{props.userNumber}</Text>.
+          </BodyText>
+        </View>
+        <PrimaryButton onPress={props.onRestart}>New Game</PrimaryButton>
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   screenContainer: {
-    backgroundColor: "#f9f9f9"
+    backgroundColor: "#f9f9f9",
   },
   screen: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#f9f9f9",
-    paddingVertical: 10
+    paddingVertical: 10,
   },
   image: {
     height: "100%",
